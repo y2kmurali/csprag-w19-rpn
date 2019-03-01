acbf6a5579f8ec5e72fac586224e9ab096543357
muralim <y2kmurali@gmail.com>
2019-03-01 10:43:37 -0500

    Added test for exponentiation, expect to fail CI

--- test_rpn.py
+++ test_rpn.py
@@ -15,3 +15,6 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_exponentiate(self):
+        result = rpn.calculate("2 8 ^")
+        self.assertEqual(256, result)
